8bc79702b25fcb5f8b9b2e88d793b1ef49c90ed9
amal-antony <amal.antony@tibilsolutions.com>
2021-10-19 14:08:44 +0530

    Changed logic of leap year and multiplication table and added notes for day2

--- Practice/AssignmentSet1/MultiplicationTableTryExcept.py
+++ Practice/AssignmentSet1/MultiplicationTableTryExcept.py
@@ -1,16 +1,19 @@
-def multTableTryCatch(num1,lmt):
-    print(f"printing multiplication table of {num1} till limit {lmt-1}")
-    for i in range(1,lmt):
-        print(f"{num1} * {i} = {num1*i}")
+def multTableTryCatch(num1,num2,lmt):
+    print(f"printing multiplication table of all numbers from  {num1} to {num2} till limit {lmt-1}\n")
+    for i in range(num1,num2+1):
+        print(f"\nMultiplication table of {i}")
+        for j in range(1,lmt):
+            print(f"{i} * {j} = {i*j}")
 
-number=int(input("enter a number to print multiplication table\n"))
+number=int(input("enter lower limit\n"))
+number2=int(input("enter upper limit\n"))
 limit=10
 try:
     limit=int(input("Will print table upto limit 10 , press Enter if u want to do so , else specify limit below\n"))
 except ValueError:
     print("Ok u didnt enter any number so will print multiplication table till 10\n")
     print("printing multiplication table till 10\n")
-multTableTryCatch(number, limit+1)
+multTableTryCatch(number,number2,limit+1)
 
 
 
